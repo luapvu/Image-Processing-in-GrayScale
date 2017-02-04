27e52f73e4ba2349de34020c2b3d2e21b5fd3a8e
luapvu <luapvu@gmail.com>
2017-02-04 15:36:12 -0800

    Reorganized main script and files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Image-Processing-in-GrayScale
+
+This project allows users to read in two coloured images of the same MxN ratio. The images are converted into gray scale and subtracted from one another. The images are then written to a jpeg file.
+
+To run this script call main.m in matlab, you can add your own images in to explore or run off my images provided.
--- src/main.m
+++ src/main.m
@@ -1,26 +1,29 @@
+addpath(genpath('functions'));
+addpath(genpath('Images'));
+
 %Read in both images
-image1 = readImg('mugshot1.jpg'); 
-image2 = readImg('mugshot2.jpg');
+image1 = readImg('mugshot1.JPG'); 
+image2 = readImg('mugshot2.JPG');
 %Calls Calculation function which converts images to gray scale and also
 %outputs the difference between the two images
 [subtractedImage, grayMugshot1, grayMugshot2] = imgDifference(image1, image2);
 
 %Write the images to 3 output files
-writeImg(grayMugshot1, 'grayMugshot1.jpg');
-writeImg(grayMugshot2, 'grayMugshot2.jpg');
-writeImg(subtractedImage, 'subtractedImgOutput.jpg');
+writeImg(grayMugshot1, 'grayMugshot1.JPG');
+writeImg(grayMugshot2, 'grayMugshot2.JPG');
+writeImg(subtractedImage, 'subtractedImgOutput.JPG');
 
 %Display these 3 output files for user to view
-figure; imshow('mugshot1.jpg');
+figure; imshow('mugshot1.JPG');
 title('Colored Mugshot 1');
-figure; imshow('mugshot2.jpg');
+figure; imshow('mugshot2.JPG');
 title('Colored Mugshot 2');
 
-figure; imshow('grayMugshot1.jpg');
+figure; imshow('grayMugshot1.JPG');
 title('Gray Scale Mugshot 1');
 
-figure; imshow('grayMugshot2.jpg');
+figure; imshow('grayMugshot2.JPG');
 title('Gray Scale Mugshot 2');
 
-figure; imshow('subtractedImgOutput.jpg');
+figure; imshow('subtractedImgOutput.JPG');
 title('Pixel difference between imgs 1 and 2');
